cc04548acb67906435da96acd65e3b434bf0addc
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-08-20 18:06:55 +0900

    Userモデルテストコード実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              { '1k1k1k' }
     password_confirmation {password}
-    first_name            { '山田' }
-    last_name             { '太郎' }
-    first_name_reading    { 'ヤマダ' }
-    last_name_reading     { 'タロウ' }
+    first_name            { '太郎' }
+    last_name             { '山田' }
+    first_name_reading    { 'タロウ' }
+    last_name_reading     { 'ヤマダ' }
     birthday              { '1999-10-10' }
     
   end
