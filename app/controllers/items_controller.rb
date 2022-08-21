da95675371a37bd146579f4a73d55f55d7b2da97
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-08-21 14:26:58 +0900

    画面遷移追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :info, presence: true
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank"} 
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :user, presence: true 
 end
