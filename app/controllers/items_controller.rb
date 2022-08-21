73df0b4da6a080d28ece22c6088ea28f7559404b
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-08-21 16:54:51 +0900

    商品新規登録の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,28 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :price, :info, :status_id, :category_id, :shipping_fee_status_id,
+      :prefecture_id, :scheduled_delivery_id
+      ).merge(user_id: current_user.id) 
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to new_user_session_path
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,15 +2,15 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   validates :name, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, format: {with: /\A[0-9]{3,7}\z/, message: 'is invalid. Input half-width characters'}
   validates :info, presence: true
   validates :status_id, numericality: { other_than: 1, message: "can't be blank"} 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" } 
   validates :scheduled_delivery_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :user, presence: true 
 
+  belongs_to :user
   belongs_to :status
   belongs_to :category
   belongs_to :shipping_fee_status
