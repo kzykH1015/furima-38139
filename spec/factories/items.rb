4579b431c2db70a55352bf92bc9c54c1351c55e9
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-08-22 14:35:17 +0900

    itemテストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name              { Faker::Name.initials(number: 2) }
+    info                 { 'wwwwwww' }
+    price              { 1000 }
+    category_id { 2 }
+    status_id            { 2 }
+    shipping_fee_status_id             { 2 }
+    scheduled_delivery_id    { 2 }
+    prefecture_id    { 2 }
+    user_id          { @user }
+
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/furima-intro01.png'), filename: 'test_image.png')
+    end
   end
 end
