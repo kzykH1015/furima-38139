efbbb4f14c3ced11d7ddf3da815584871b5876e0
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-08-22 16:33:20 +0900

    出品機能コード修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,8 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   validates :name, presence: true
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' },
-                    numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, numericality: {only_integer: true, message: 'is invalid. Input half-width characters'}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :info, presence: true
   validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
