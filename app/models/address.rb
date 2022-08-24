2313cf12e41f8ec4baefd1941f2cb2d88f326b87
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-08-25 02:39:21 +0900

    バリデーションの記述

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,10 @@
 class Address < ApplicationRecord
+  validates :postal_cord, presence: true
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :building
+  validates :phone_number, presence: true
+
+  belongs_to :prefecture
 end
