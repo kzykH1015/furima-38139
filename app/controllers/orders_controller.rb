6207e4daf558e4f473c82bf0f7d5f037f0a738e6
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-08-25 03:14:58 +0900

    購入ページの画面遷移調整

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :item_setting, only: :index
+  before_action :move_to_login
+
   def index
+    redirect_to root_path if @item.user == current_user
   end
+
+  private
+
+  def item_setting
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_login
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
 end
