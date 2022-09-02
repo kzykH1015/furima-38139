4f9f6bc1698e0cb11283676846f56aaacf69016c
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-09-02 15:00:29 +0900

    カード登録ファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'cards/new'
   devise_for :users
   root "items#index"
   resources :items do
     resources :orders, only: [:index, :create]
   end
   resources :addresses, only: [:new, :create]
+  resources :cards, only: [:new, :create]
 end
