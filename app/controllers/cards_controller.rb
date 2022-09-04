85c7092fb371ae708084f838148b94938ac0ad97
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-09-04 15:54:17 +0900

    Revert "カード登録時のエラーメッセージ実装"
    This reverts commit 7bc98386c9b8cfdf345725fc3b0fa29b3f278a84.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,23 +1,22 @@
 class CardsController < ApplicationController
   def new
-    @card = Card.new
   end
 
   def create
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     customer = Payjp::Customer.create(
-      description: 'test',
+      description: 'test', 
       card: params[:card_token]
     )
-    @card = Card.new(
+    card = Card.new(
       card_token: params[:card_token],
       customer_token: customer.id,
       user_id: current_user.id
     )
-    if @card.save
+    if card.save
       redirect_to root_path
     else
-      render :new
+      redirect_to action: "new"
     end
   end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -42,7 +42,7 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     customer_token = current_user.card.customer_token
     Payjp::Charge.create(
       amount: @item.price, # 商品の値段
@@ -56,7 +56,7 @@ class OrdersController < ApplicationController
   end
 
   def card_find
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY'] # 環境変数を読み込む
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] # 環境変数を読み込む
     card = Card.find_by(user_id: current_user.id)
 
     customer = Payjp::Customer.retrieve(card.customer_token) # 先程のカード情報を元に、顧客情報を取得
