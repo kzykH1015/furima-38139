ea40c5ae2b019828dfd7bd6651ac28bc3bf6c325
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-09-04 15:55:04 +0900

    rubocopの適用

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,9 +3,9 @@ class CardsController < ApplicationController
   end
 
   def create
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     customer = Payjp::Customer.create(
-      description: 'test', 
+      description: 'test',
       card: params[:card_token]
     )
     card = Card.new(
@@ -16,7 +16,7 @@ class CardsController < ApplicationController
     if card.save
       redirect_to root_path
     else
-      redirect_to action: "new"
+      redirect_to action: 'new'
     end
   end
 end
