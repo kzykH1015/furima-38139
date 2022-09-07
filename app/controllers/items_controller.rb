ccdf1b7574fe593c4e69ee180cacbfa94fc8b19d
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-09-07 19:59:24 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,9 +50,10 @@ class ItemsController < ApplicationController
   end
 
   def search
-    return nil if params[:keyword] == ""
+    return nil if params[:keyword] == ''
+
     tag = Tag.where(['tag_name LIKE?', "%#{params[:keyword]}%"])
-    render json:{ keyword: tag}
+    render json: { keyword: tag }
   end
 
   private
