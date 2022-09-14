de0fb85604ed64e0276c5118a6ee2f8b2cc8d04d
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-09-14 19:29:11 +0900

    コメント一覧の作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,19 @@
 class CommentsController < ApplicationController
   def show
     @comments = @item.comments.includes(:user)
-    @Comment = Comment.new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to item_path(params[:item_id])
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
