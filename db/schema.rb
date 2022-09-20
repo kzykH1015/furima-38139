3f838449ed0de2b0e3c4d7f4a044aa678553cc92
kzykH1015 <108124770+kzykH1015@users.noreply.github.com>
2022-09-21 04:45:56 +0900

    不必要なカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_19_175717) do
+ActiveRecord::Schema.define(version: 2022_09_20_194246) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -115,7 +115,6 @@ ActiveRecord::Schema.define(version: 2022_09_19_175717) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
